7cc20f835037b1d6804d808028c81e9da3754b3d
discrimine <discrimine96@gmail.com>
2018-07-26 15:19:07 +0300

    cli commands

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+
+import { RouteRoutingModule } from './route/route-routing.module';
+import { RouterModule } from '../../node_modules/@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
